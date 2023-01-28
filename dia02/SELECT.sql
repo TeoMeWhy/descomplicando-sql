aa7900d99840529d09d832538f8751cd0352539e
teomewhy@gmail.com <teomewhy@gmail.com>
2023-01-28 17:11:37 +0000

    dia02 com SELECT

---
+++ dia02/SELECT.sql
@@ -0,0 +1,33 @@
+-- Databricks notebook source
+-- DBTITLE 1,Primeiro comando!
+SELECT 'Olá, Mundo!'
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Somando coisas
+SELECT 2 + 2
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Multiplicando
+SELECT 2 * 97
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Calculando a potência
+SELECT POW(10,2)
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Resto de divisão
+SELECT 9 % 2
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Resto da divisão novamente
+SELECT MOD(9,2)
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Concatenando textos
+SELECT 'Olá, ' || 'Téo Calvo!'
